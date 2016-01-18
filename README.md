6d0786109bdd5bc2d6123f47325230a9f1832620
Nathan <nathanjdonato@gmail.com>
2016-01-17 20:28:35 -0500

    Set up

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# getting machine up to speed
-
-From repo:
-
-`sudo easy_install virtualenv`
-
-`virtualenv env`
-
-`source env/bin/activate`
+simple daily journal
---
+++ dandelines.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, json, request
+from flask.ext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+mysql = MySQL()
+ 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'dande'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+# Routes
+@app.route("/")
+def main():
+    return render_template('index.html')

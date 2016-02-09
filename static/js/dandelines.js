665f3556f9d771ae3280ff7176d00d5a5b4d4d38
nathandonato <nathanjdonato@gmail.com>
2016-02-08 22:17:40 -0500

    Moved stuff to static files

---
+++ static/js/dandelines.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#summernote').summernote({
+    });
+
+    $(".frosted-glass").appendTo(".note-editing-area");
+    $(".note-editable").appendTo(".note-editing-area");
+});

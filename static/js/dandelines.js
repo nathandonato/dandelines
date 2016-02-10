a789d9bdbdd0933c41a1d19d36833fe6132e58a8
nathandonato <nathanjdonato@gmail.com>
2016-02-09 20:00:41 -0500

    Set editor size dynamically

--- static/js/dandelines.js
+++ static/js/dandelines.js
@@ -2,6 +2,24 @@ $(document).ready(function() {
     $('#summernote').summernote({
     });
 
+    setElementHeight(".note-editing-area", getHeightByWindow());
     $(".frosted-glass").appendTo(".note-editing-area");
     $(".note-editable").appendTo(".note-editing-area");
+
+    $(".note-statusbar").on("click", function(){
+    	$(".panel-body").css({position: relative});
+    });
 });
+
+$(window).resize(function(){
+	console.log("resize");
+    setElementHeight(".note-editing-area", getHeightByWindow());
+})
+
+function setElementHeight(element, size) {
+	$(element).height(size);
+}
+
+function getHeightByWindow() {
+  	return $(document).height() - 273;// - $(".panel-heading").height();
+}

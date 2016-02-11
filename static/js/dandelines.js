8e29aad86786d4ccf68eb5a9fe2c6a2cef19c27d
nathandonato <nathanjdonato@gmail.com>
2016-02-11 01:59:49 -0500

    Added autosave

--- static/js/dandelines.js
+++ static/js/dandelines.js
@@ -1,3 +1,6 @@
+var today = Date.create();
+var contents;
+
 $(document).ready(function() {
     // Set background image
     setBackgroundImage();
@@ -8,7 +11,8 @@ $(document).ready(function() {
     $('#summernote').summernote({
     });
 
-    postData();
+    // Get journal
+    getTodaysJournal();
 
     // Set editor's size
     setElementHeight(".note-editing-area", getHeightByWindow());
@@ -21,6 +25,15 @@ $(document).ready(function() {
 
     // Remove the manual resize option
     $(".note-statusbar").remove();
+
+    // Autosave
+    var timeoutId;
+    $(".note-editable").on("input", function(){
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function() {
+            saveToDB();
+        }, 500);
+    });
 });
 
 $(window).resize(function(){
@@ -46,21 +59,43 @@ function setBackgroundImage() {
 }
 
 function setToday() {
-    $("#today").text(Date.create().format('{Month} {d}, {yyyy}'));
+    $("#today").text(today.format('{Month} {d}, {yyyy}'));
 }
 
 function setElementHeight(element, size) {
     $(element).height(size);
 }
 
-function postData(){
-    // POST 
-    // Since this deals with backend and the rest is strictly front end, consider moving to another file?
-    // Nah prob not
+function getTodaysJournal(){
+    getJournal({ user_id: 1, entry_date: today.format('{yyyy}-{MM}-{dd}'), create_if_null: 'yes' });
 }
 
-function getData(){
-    // GET
-    // Since this deals with backend and the rest is strictly front end, consider moving to another file?
-    // Nah prob not
+function getJournal(params){
+    $.post("/getJournal", params, function(data){
+        $(".note-editable").contents().replaceWith(data);
+    });
+    // TODO: Add a getJournal function for any date (but don't allow editing past dates)
+}
+
+function saveToDB(){
+    newHtml = $(".note-editable").html();
+    $.put("/writeJournal", { 'user_id': 1, 'entry_date': today.format('{yyyy}-{MM}-{dd}'), 'entry': newHtml }, function(data){
+        console.log("Saving...");
+    });
+}
+
+// Make a put
+$.put = function(url, data, callback, type){
+  if ( $.isFunction(data) ){
+    type = type || callback,
+    callback = data,
+    data = {}
+  }
+  return $.ajax({
+    url: url,
+    type: 'PUT',
+    success: callback,
+    data: data,
+    contentType: type
+  });
 }
